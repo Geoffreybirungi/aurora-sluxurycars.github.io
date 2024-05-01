b3807e4ca4f582b764342f24069d78c6ac6bd67e
Geoffreybirungi <146008160+Geoffreybirungi@users.noreply.github.com>
2024-05-01 16:19:43 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+let slideIndex = 0;
+const slides = document.getElementsByClassName('slide');
+
+function showSlides() {
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) { slideIndex = 1 }
+  slides[slideIndex - 1].style.display = 'block';
+  setTimeout(showSlides, 4000); // Change image every 4 seconds
+}
+
+window.onload = function() {
+  showSlides();
+};
+
+
+
+
